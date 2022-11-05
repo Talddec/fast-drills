a575707bbb4c9b8b770bfe3f9c07bc0d75fab9da
Talddec <85902255+Talddec@users.noreply.github.com>
2022-11-05 21:55:40 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 Events.on(ClientLoadEvent, b  => {
-Blocks.mechanicalDrill.drillTime=0.000005
-Blocks.pneumaticDrill.drillTime=0.0000005
-Blocks.laserDrill.drillTime=0.0000005
-Blocks.blastDrill.drillTime=0.0000005
+Blocks.mechanicalDrill.drillTime=0
+Blocks.pneumaticDrill.drillTime=0
+Blocks.laserDrill.drillTime=0
+Blocks.blastDrill.drillTime=0
 });
